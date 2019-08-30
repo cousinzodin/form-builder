ef12655bd0c381be6da760aeecb383f12c281fc9
Alexandra Gor <alexandra.goryacheva@streamtechltd.com>
2019-08-30 15:43:34 +0300

    Updates

---
+++ precache-manifest.139e781b5a6892884e85d1fc1145db5e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b779bb455e09aef9bd4017f09cc0624d",
+    "url": "/form-builder/index.html"
+  },
+  {
+    "revision": "583fe59054f84aed9a83",
+    "url": "/form-builder/static/js/2.0413cb6f.chunk.js"
+  },
+  {
+    "revision": "00a5da03f18304f9a8e5",
+    "url": "/form-builder/static/js/main.3c48de7c.chunk.js"
+  },
+  {
+    "revision": "eb2706e1cd5333de37e4",
+    "url": "/form-builder/static/js/runtime~main.51562bb3.js"
+  }
+]);
